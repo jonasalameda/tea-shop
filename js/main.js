8ef8fa73c3c20600991f56ecdb5fee4784c722ba
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-04-22 18:23:17 -0700

    feat: add fetch data function for API

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
 
 }
+
+async function fetchData(resourceUrl) {
+    try {
+        const response = await fetch(resourceUrl, {
+            method: "GET",
+        })
+    
+        if (!response.ok) {
+            throw new Error(`An error occurred while processing: ${response.status}`)
+        }
+        
+        // Retrieve the payload from the response
+        const data = await response.json()
+    
+        return data
+    } catch (error) {
+        throw error
+    }
+}
