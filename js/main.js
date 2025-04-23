1f951eb93b87933e504cd07a77e4cd2e5cf64e8b
%username% <%email%>
2025-04-23 18:05:18 -0400

    made sure all pages had the data-page, made the account page now all pages should be interactiong with eachother perfectly👍

--- js/main.js
+++ js/main.js
@@ -3,9 +3,37 @@ import { initProducts } from "./modules/productDetails.js";
 
 
 document.addEventListener("DOMContentLoaded", initApp);
-
+const docPage = document.querySelector("[data-page]");
 function initApp() {
+
     console.log("App initialized");
+
+    switch(docPage){
+        case "homePage":
+            console.log("Home page loaded");
+            initProducts();
+            break;
+        case "productDetailPage":
+            console.log("Product detail page loaded");
+            initProductDetails();
+            break;
+        case "cartPage":
+            console.log("Cart page loaded");
+            initCart();
+            break;
+        case "conatctPage":
+            console.log("Contact page loaded");
+            initContact();
+            break;
+        case "aboutUsPage":
+            console.log("About us page loaded");
+            initAboutUs();
+            break;
+        case "loginPage":
+            console.log("Login page loaded");
+            break;
+
+    }
 
     initProducts();
     // const cartButton = document.getElementById("cart-button");
